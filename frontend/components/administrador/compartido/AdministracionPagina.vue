069d2ccf71df83e37019a7ef95eb0213db4d7733
William Condori Quispe <williamccondori@outlook.com>
2023-11-30 09:41:17 -0500

    Fix imports

--- frontend/components/administrador/compartido/AdministracionPagina.vue
+++ frontend/components/administrador/compartido/AdministracionPagina.vue
@@ -1,21 +1,15 @@
 <template>
-    <ACard class="app--altura-100">
+    <a-card class="app--altura-100">
         <div v-if="titulo">
-            <APageHeader :title="titulo" @back="() => $router.back()" />
-            <ADivider />
+            <a-page-header :title="titulo" @back="() => $router.back()" />
+            <a-divider />
         </div>
         <slot />
-    </ACard>
+    </a-card>
 </template>
 
 <script>
-import { Card, PageHeader, Divider } from 'ant-design-vue';
 export default {
-    components: {
-        ACard: Card,
-        APageHeader: PageHeader,
-        ADivider: Divider,
-    },
     props: {
         titulo: {
             type: String,
